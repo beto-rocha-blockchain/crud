d86220cb7d78266f99fe33f2a37a5ee434b41625
Beto Rocha <roberto.blockchainresources@gmail.com>
2025-07-28 22:21:54 -0300

    1

--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug)]
 
 pub struct DataEntry {
-    pub data1: Vec<String>,
-    pub data2: Vec<u8>,
+    pub func_names: Vec<String>,
+    pub bytecode: Vec<u8>,
     pub owner: String,
 }
